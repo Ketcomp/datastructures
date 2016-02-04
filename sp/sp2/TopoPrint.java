843a2e2d3f0bd655a7dca3fc56c7eb1cee967099
Gaurav <gketkar08@gmail.com>
2016-02-03 18:32:30 -0600

    algo2 works. Yet to implement algo 1

--- sp/sp2/TopoPrint.java
+++ sp/sp2/TopoPrint.java
@@ -1,4 +1,10 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * @author: Gaurav Ketkar
+ */
 
 public class TopoPrint<T>{
 	
@@ -9,22 +15,50 @@ public class TopoPrint<T>{
        */
    //}
 
+	public static void dfsVisit(Vertex u, Stack<Vertex> S) {
+		u.seen = true;
+		Vertex v;
+		for(Edge e: u.Adj) {
+			v = e.otherEnd(u);
+			if(!v.seen){
+				v.parent = u;
+				dfsVisit(v, S);
+			}
+		}
+		S.push(u);
+	}
 
-
-//Stack<Vertex> toplogicalOrder2(Graph g) {
+	public static Stack<Vertex> toplogicalOrder2(Graph g) {
       /* Algorithm 2. Run DFS on g and push nodes to a stack in the
        order in which they finish.  Write code without using global variables.
        */
-  // }
+		for(Vertex u : g) {
+			u.seen = false;
+			u.parent = null;
+		}
+		Stack<Vertex> S = new Stack<>();
+		for (Vertex u : g) {
+			if(!u.seen) dfsVisit(u, S);
+		}
+		return S;
+	}
 
 
 	public static void main(String[] args){
 		Graph myGraph;
 		Scanner in = new Scanner(System.in);
 		boolean isDirected = true;
-		if(args.length!=0){
-			if(args[2]=="false") isDirected = false;
+		
+		myGraph = Graph.readGraph(in, isDirected);
+		
+		Stack<Vertex> opAlgo2 = new Stack<>();
+		
+		opAlgo2 = toplogicalOrder2(myGraph);
+		//opAlgo1 = toplogicalOrder1(myGraph);
+		int sizeofstack = opAlgo2.size();
+		System.out.println("Topo ordering by algo 2:");
+		for(int i =0; i<sizeofstack;i++) {
+			System.out.print(opAlgo2.pop().toString() +", ");
 		}
-		myGraph.readGraph(in, isDirected);
 	}
 }//Class ends
