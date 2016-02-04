1b114114a3d9d354bac94bc61a0d993c898b3d3e
Gaurav <gketkar08@gmail.com>
2016-02-03 19:30:46 -0600

    Just removing edges that x can visit remains to be implemented

--- sp/sp2/TopoPrint.java
+++ sp/sp2/TopoPrint.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.util.List;
 import java.util.Stack;
+import java.util.ArrayList;
 
 /**
  * @author: Gaurav Ketkar
@@ -9,11 +10,27 @@ import java.util.Stack;
 public class TopoPrint<T>{
 	
 
-	//List<Vertex> toplogicalOrder1(Graph g) { 
+	public static List<Vertex> toplogicalOrder1(Graph g) { 
       /* Algorithm 1. Remove vertices with no incoming edges, one at a
        time, along with their incident edges, and add them to a list.
        */
-   //}
+	List<Vertex> result = new ArrayList<>();
+	for(Vertex every : g) {
+		if(every.revAdj.size()==0) {
+			//Remove the incoming edges for vertices 'every' goes to
+			for(Edge removeThisEdge : every.Adj) {
+				Vertex candidateVertex = removeThisEdge.To;
+				for(Edge e2 : candidateVertex.revAdj) {
+					if(e2.From.equals(every)){
+						candidateVertex.revAdj.remove(index)
+					}
+				}
+			}
+			result.add(every);
+		}
+	}
+		return result;
+	}
 
 	public static void dfsVisit(Vertex u, Stack<Vertex> S) {
 		u.seen = true;
@@ -52,13 +69,19 @@ public class TopoPrint<T>{
 		myGraph = Graph.readGraph(in, isDirected);
 		
 		Stack<Vertex> opAlgo2 = new Stack<>();
-		
 		opAlgo2 = toplogicalOrder2(myGraph);
-		//opAlgo1 = toplogicalOrder1(myGraph);
-		int sizeofstack = opAlgo2.size();
+		int sizeofstack2 = opAlgo2.size();
 		System.out.println("Topo ordering by algo 2:");
-		for(int i =0; i<sizeofstack;i++) {
+		for(int i =0; i<sizeofstack2;i++) {
 			System.out.print(opAlgo2.pop().toString() +", ");
+		}
+		
+		List<Vertex> opAlgo1 = new ArrayList<>();
+		opAlgo1 = toplogicalOrder1(myGraph);
+		int sizeofstack1 = opAlgo1.size();		
+		System.out.println("Topo ordering by algo 1:");
+		for(int i =0; i<sizeofstack1;i++) {
+			System.out.print(opAlgo1.toString()+", ");
 		}
 	}
 }//Class ends
