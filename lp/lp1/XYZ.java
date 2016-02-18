855bafb84ccb1cf0ec8b513562424e22a992f1cd
Madhuri Abnave <Madhuri Abnave>
2016-02-17 21:13:17 -0600

    Add lp1 and its classes

---
+++ lp/lp1/XYZ.java
@@ -0,0 +1,81 @@
+/**
+ * 
+ */
+package lp.lp1;
+
+/*
+ @author Madhuri
+ */
+public class XYZ {
+	
+
+	XYZ(String num)
+	{
+		
+	}
+	XYZ(long num)
+	{
+		
+	}
+	@Override
+	public String toString()
+	{
+		//convert this to decimal
+		return null;
+	}
+	
+	public XYZ add(XYZ a, XYZ b)
+	{
+		return null;
+	}
+	
+	public XYZ subtract(XYZ a, XYZ b)
+	{
+		
+		//if a < b
+		return new XYZ(0);
+	}
+	
+	 public XYZ product(XYZ a, XYZ b)
+	 {
+		 return null;
+	 }
+	
+	public XYZ power(XYZ a, long n)
+	{
+		return null;
+	}
+	
+	public void printList()
+	{
+		//print base : + list elements in lsd to msd order
+	}
+	//level 2 started
+	//a and n are both XYZ. Here a may be negative, but assume that n is non-negative.
+	public XYZ power(XYZ a, XYZ n)
+	{
+		return null;
+	}
+	// Fractional part is discarded (take just the quotient). Both a and b may be positive or negative. If b is 0, raise an exception.
+	public XYZ divide(XYZ a, XYZ b)
+	{
+		return null;
+	}
+	
+	private XYZ divideByTwo(XYZ a)
+	{
+		return null;
+	}
+	
+	
+	//remainder you get when a is divided by b (a%b). Assume that a is non-negative, and b > 0.
+	public XYZ mod(XYZ a, XYZ b)
+	{
+		return null;
+	}
+	// return the square root of a (truncated). Use binary search. Assume that a is non-negative
+	public XYZ squareRoot(XYZ a)
+	{
+		return null;
+	}
+}
