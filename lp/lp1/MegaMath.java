2de95864e5e0a58cff86d8717645b276818bf050
ketcomp <gketkar08@gmail.com>
2016-02-28 01:06:51 -0600

    Remove variable digit and hopefully reduce copying

--- lp/lp1/MegaMath.java
+++ lp/lp1/MegaMath.java
@@ -1,113 +1,151 @@
-
-
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 /*
- @author Madhuri
+ @author Group 10
  */
+
 public class MegaMath {
-	
-	final int base=10;
+	// assume default base is 10
+	static int base = 10;
 	List<Integer> number = new ArrayList<Integer>();
-	
-	MegaMath()
-	{
-		
+
+	// What does this constructor do?
+	MegaMath() {
+
 	}
-	
+
+	// Constructor for MegaMath class; takes a string s as parameter, that
+	// stores a number in decimal,
+	// and creates the MegaMath object representing that number.
+	// The string can have arbitrary length.
 	MegaMath(String num) {
-
 		for (int i = num.length() - 1; i >= 0; i--) {
-			int temp = Integer.parseInt(String.valueOf(num.charAt(i)));
-			number.add(new Integer(temp));
+			number.add(Integer.parseInt(String.valueOf(num.charAt(i))));
 		}
-
 	}
 
 	MegaMath(Long num) {
-
 		this(num.toString());
-
 	}
 
+	// Convert the MegaMath class object into its equivalent string (in
+	// decimal).
+	// There are no leading zeroes in the string.
 	@Override
 	public String toString() {
 		// convert this to decimal
 		String s = "";
-		Iterator itr = this.number.iterator();
+		Iterator<Integer> itr = this.number.iterator();
 		while (itr.hasNext()) {
 			s = s + itr.next().toString();
 		}
-		return s;
+		// return the number in human readable order
+		return new StringBuilder(s).reverse().toString();
 	}
 
+	// Sum of two numbers stored as MegaMath.
 	public static MegaMath add(MegaMath a, MegaMath b) {
 		List<Integer> number = new ArrayList<>();
 		List<Integer> num1 = a.number;
 		List<Integer> num2 = b.number;
-		Iterator itr1 = num1.iterator();
-		Iterator itr2 = num2.iterator();
-
+		Iterator<Integer> itr1 = num1.iterator();
+		Iterator<Integer> itr2 = num2.iterator();
 		int carry = 0;
-
+		// Iterate through both numbers and add them digit by digit.
 		while (itr1.hasNext() && itr2.hasNext()) {
 			int sum = 0;
-			int temp = (Integer) itr1.next() + (Integer) itr2.next();
+			int temp = (int) itr1.next() + (int) itr2.next();
 			sum = temp + carry;
-			number.add(sum % 10);
-			carry = sum / 10;
+			number.add(sum % base);
+			carry = sum / base;
 		}
-
+		// At this point either iter1 or iter2 has run out of elements
 		while (itr1.hasNext()) {
 			int sum = 0;
-			int temp = (Integer) itr1.next();
+			int temp = (int) itr1.next();
 			sum = temp + carry;
-			number.add(sum % 10);
-			carry = sum / 10;
+			number.add(sum % base);
+			carry = sum / base;
 		}
 		while (itr2.hasNext()) {
 			int sum = 0;
-			int temp = (Integer) itr2.next();
+			int temp = (int) itr2.next();
 			sum = temp + carry;
-			number.add(sum % 10);
-			carry = sum / 10;
+			number.add(sum % base);
+			carry = sum / base;
 		}
-		MegaMath add = new MegaMath();
-		add.number = number;
-
-		return add;
+		MegaMath additionResult = new MegaMath();
+		additionResult.number = number;
+		return additionResult;
 	}
 
-	public MegaMath subtract(MegaMath a, MegaMath b) {
-
-		// if a < b
-		return new MegaMath();
+	// Difference of two MegaMath numbers returned if positive.
+	public static MegaMath subtract(MegaMath a, MegaMath b) {
+		List<Integer> number = new ArrayList<>();
+		List<Integer> num1 = a.number;
+		List<Integer> num2 = b.number;
+		Iterator<Integer> itr1 = num1.iterator();
+		Iterator<Integer> itr2 = num2.iterator();
+		MegaMath result = new MegaMath();
+		int borrow = 0;
+		int carry = 0;
+		
+		// if a < b return 0
+		if( compare(a,b)<0 ) return new MegaMath("0");
+		
+		// else subtract
+		// Iterate through both numbers and subtract them digit by digit.
+		while (itr1.hasNext() && itr2.hasNext()) {
+			int one = (int) itr1.next();
+			int two = (int) itr2.next();
+			int temp= 0;
+			if( one-borrow-two < 0 ){
+				temp = one+10+carry - borrow  - two;
+				borrow = 1;
+			}
+			else {
+				temp = one+carry - borrow - two;
+				borrow = 0;
+			}
+			number.add(temp % base);
+			carry = temp / base;
+		}
+		// At this point, iter2 has run out of elements
+		while (itr1.hasNext()) {
+			int temp = (int) itr1.next()+carry - borrow;
+			if (temp == 0) break; // Prevents leading zeros from appearing.
+			number.add(temp % base);
+			carry = temp / base;
+		}
+		result.number = number;
+		return result;
 	}
 
+	// Product of two numbers.
 	public MegaMath product(MegaMath a, MegaMath b) {
 		return null;
 	}
 
+	// Number 'a' raised to power 'n'
 	public MegaMath power(MegaMath a, long n) {
 		return null;
 	}
 
+	// Print the base + ":" + elements of the list, separated by spaces.
 	public void printList() {
-		
-		Iterator iter= this.number.iterator();
-		Integer digit;
-		System.out.print(base+" : ");
-		while(iter.hasNext())
-		{
-			digit= (Integer)iter.next();
-			System.out.print(digit+" ");
+		Iterator<Integer> iter = this.number.iterator();
+		System.out.print(base + " : ");
+		while (iter.hasNext()) {
+			// Number is printed in reverse - LSD first.
+			System.out.print((Integer) iter.next() + " ");
 		}
 	}
 
 	// level 2 started
-	// a and n are both MegaMath. Here a may be negative, but assume that n is
+	// 'a' and 'n' are both MegaMath. Here 'a' may be negative, but assume that
+	// 'n' is
 	// non-negative.
 	public MegaMath power(MegaMath a, MegaMath n) {
 		return null;
