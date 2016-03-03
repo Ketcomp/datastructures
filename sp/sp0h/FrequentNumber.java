f69f599c2ccc6550dccbba239dc4d4f43b34e994
ketcomp <gketkar08@gmail.com>
2016-03-03 08:53:44 -0600

    Include comments, improve redability. Rename max_count to current_max etc.

--- sp/sp0h/FrequentNumber.java
+++ sp/sp0h/FrequentNumber.java
@@ -1,110 +1,102 @@
 /*
- * Author@Malav Shah
- * 
- * Team:
- * Gaurav Ketkar
- * Madhuri Abnave
- * Vijay Mungara
- */
+* @author: Malav Shah
+* Team: G10
+* Gaurav Ketkar
+* Madhuri Abnave
+* Vijay Mungara
+* Malav Shah
+*/
 
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+
+
 /*
- * 'mostFrequent(int[] array)' takes as parameter an array of integers, and returns 
- * an integer that is most frequent in the array.
+ * An O(nlogn) algorithm that sorts the array with Arrays.sort
  */
 public class FrequentNumber {
-	
-	public static int mostFrequent(int[] arr)
-	{
-		HashMap<Integer, Integer> frequency_map= new HashMap<Integer, Integer>();
-		int length= arr.length;
-		int count=0;
-		//Storing the count of each integer in the hashmap
-		for(int i=0;i<length;i++)
-		{
-			if(frequency_map.get(arr[i])==null)
-			{
-				frequency_map.put(arr[i], 1);
-			}
-			else
-			{
-				count= frequency_map.get(arr[i]);
-				count++;
-				frequency_map.put(arr[i], count);
-			}
-		}
+
+	public static int frequencyBySort(int arr[]) {
+		// Sorting the list.
+		Arrays.sort(arr);
+
+		int current_count = 1;
+		int current_max = 0;
+		int max_number = 0;
 		
-		//Iterating over the hashmap to find the most frequent number.
-		int max_count=0;
-		int max_number=0;
-		Map.Entry<Integer, Integer> pair;
-		Iterator iter= frequency_map.entrySet().iterator();
-		while(iter.hasNext())
-		{
-			pair= (Map.Entry<Integer, Integer>)iter.next();
-			if(pair.getValue()>= max_count)
-			{
-				max_count= pair.getValue();
-				max_number=pair.getKey();
+		// Iterating over the sorted list to find the most frequent number.
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] == arr[i - 1]) {
+				current_count++;
+			}else {
+				current_count = 1; // Re-init current_count for the next number in the sorted array.
+			}
+			if (current_count >= current_max) {
+				current_max = current_count;
+				max_number = arr[i - 1];
 			}
 		}
 		return max_number;
 	}
 
-	public static int frequencyBySort(int arr[])
-	{
-		//Sorting the list.
-		Arrays.sort(arr);
-		int current_count=1;
-		int max_count=0;
-		int max_number=0;
-		int length= arr.length;
+	public static void main(String args[]) {
+		int n = 200000000;
+		// User input if specified.
+		if (args.length > 0) {
+			n = Integer.parseInt(args[0]);
+		}
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++)
+			arr[i] = (int) (Math.random() * 101);
+
+		// Finding most frequent element by HashMap
+		double start = System.currentTimeMillis();
+		int max_number = mostFrequent(arr);
+		double end = System.currentTimeMillis();
+
+		System.out.println("The most frequent number using HashMap is " + max_number);
+		System.out.println("Time: " + (end - start)/1000 + "s.\n");
+
+		// Finding most frequent element by Arrays.sort
+		start = System.currentTimeMillis();
+		int max_number2 = frequencyBySort(arr);
+		end = System.currentTimeMillis();
 		
-		//Iterating over the sorted list to find the most frequent number.
-		for(int i=1;i<length;i++)
-		{
-			if(arr[i]==arr[i-1])
-			{
-				current_count++;
+		System.out.println("The most frequent number by Arrays.sort is " + max_number2);
+		System.out.println("Time: " + (end - start)/1000 + "s.");
+	}
+	
+	/*
+	 * 'mostFrequent(int[] array)' takes as parameter an array of integers, and returns 
+	 * an integer that is most frequent in the array.
+	 */
+	public static int mostFrequent(int[] arr) {
+		HashMap<Integer, Integer> freq_map = new HashMap<Integer, Integer>();
+		int length = arr.length;
+		int count = 0;
+		// Storing the count of each integer in the hashmap
+		for (int i = 0; i < length; i++) {
+			if (freq_map.get(arr[i]) == null) {freq_map.put(arr[i], 1);}
+			else {
+				count = freq_map.get(arr[i]);
+				count++; freq_map.put(arr[i], count);
 			}
-			else
-			{
-				current_count=1;
-			}
-			if(current_count>=max_count)
-			{
-				max_count=current_count;
-				max_number=arr[i-1];
+		}
+		// Iterating over the hashmap to find the most frequent number.
+		int current_max = 0;
+		int max_number = 0;
+		Map.Entry<Integer, Integer> pair;
+		Iterator iter = freq_map.entrySet().iterator();
+		
+		while (iter.hasNext()) {
+			pair = (Map.Entry<Integer, Integer>) iter.next();
+			if (pair.getValue() >= current_max) {
+				current_max = pair.getValue();
+				max_number = pair.getKey();
 			}
 		}
 		return max_number;
-	}
-	
-	public static void main(String args[])
-	{
-		int n = 100000000;
-		// User input if specified.
-		if(args.length>0) { n = Integer.parseInt(args[0]); } 
-		
-		int[] arr= new int[n];
-		for(int i=0;i<n;i++)
-			arr[i]=(int)(Math.random()*101);
-		
-		// Finding most frequent element by HashMap
-		double start= System.currentTimeMillis();
-		int max_number= mostFrequent(arr);
-		double end= System.currentTimeMillis();
-		System.out.println("The most frequent number using HashMap is " + max_number);
-		System.out.println("Time(Milliseconds): " + (end-start));
-		
-		// Finding most frequent element by Arrays.sort
-		start= System.currentTimeMillis();
-		int max_number2= frequencyBySort(arr);
-		end= System.currentTimeMillis();
-		System.out.println("The most frequent number by Arrays.sort is " + max_number2);
-		System.out.println("Time(Milliseconds): " + (end-start));
 	}
 }
