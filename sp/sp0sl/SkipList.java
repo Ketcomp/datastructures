9eb94a0c9e59c96f0b84964da40b046c8dedbd16
ketcomp <gketkar08@gmail.com>
2016-03-25 18:54:01 -0500

    change structure

--- sp/sp0sl/SkipList.java
+++ sp/sp0sl/SkipList.java
@@ -1,22 +1,35 @@
 
 /** See  http://en.wikipedia.org/wiki/Skip_list
  */
-
+/*
+* @author: Gaurav
+* Team: G10
+* Gaurav Ketkar
+* Madhuri Abnave
+* Vijay Mungara
+* Malav Shah
+*/
 import java.lang.Comparable;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Random;
 import java.util.Scanner;
-import java.util.List;
+
 public class SkipList<T extends Comparable<? super T>> {
-	static int maxLevel = 0;
-	static SLE head;
-	static SLE tail;
+	int maxLevel;
+	private SLE head;
+	private SLE tail;
+	//Class SkipList's constructor
+	private SkipList(){
+		head = null;
+		tail = null;
+		maxLevel = 0;
+	}
 	
 	/*
-	 * An object of class SkipListEntry (SLE) represents an element of the skip list
+	 * An object of class SkipListEntry (SLE) represents an element of the skip
+	 * list
 	 */
-	public static class SLE<Comparable> {
+	public class SLE<Comparable> {
 		SLE prev;
 		SLE[] next;
 		int lev;
@@ -34,22 +47,25 @@ public class SkipList<T extends Comparable<? super T>> {
 	 */
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		maxLevel = in.nextInt();
-		head = new SLE(Integer.MIN_VALUE, maxLevel);
-		tail = new SLE(Integer.MAX_VALUE, maxLevel);
-		while(in.hasNext()){
+		SkipList<Integer> skList = new SkipList<Integer>();
+		// Initialize the list
+		skList.maxLevel = in.nextInt();
+		skList.head.element = Integer.MIN_VALUE;
+		skList.tail.element = Integer.MAX_VALUE;	
+		
+		while (in.hasNext()) {
 			Integer element = in.nextInt();
-			add(element);
+			skList.add(element);
 		}
-
+	in.close();
 	}
 
-	SLE find(T x) {
-		int num =(Integer)x;
+	SLE<?> find(T x) {
+		int num = (Integer) x;
 		SLE prev;
 		SLE p = head;
-		for(int i = maxLevel; i>0; i--){
-			while(p.next[i].element < num){
+		for (int i = maxLevel; i > 0; i--) {
+			while (p.next[i].element < num) {
 				p = p.next[i];
 			}
 		}
@@ -58,20 +74,18 @@ public class SkipList<T extends Comparable<? super T>> {
 	}
 
 	/*
-	 * Add an element x to the list. Returns true if x was a
-	 * new element.
+	 * Add an element x to the list. Returns true if x was a new element.
 	 */
-	boolean add(T x) { 
+	boolean add(T x) {
 		SLE prev = find(x);
-		int val = (Integer)x;
-		if(prev.next[0].element == (Integer)x){
+		int val = (Integer) x;
+		if (prev.next[0].element == (Integer) x) {
 			prev.next[0].element = val;
 			return false;
-		}
-		else{
+		} else {
 			int lev = choice(maxLevel);
-			SLE newEntry = new SLE(val, lev);
-			for(int i= 0; i<lev; i++){
+			SLE newEntry = new SLE<Object>(val, lev);
+			for (int i = 0; i < lev; i++) {
 				newEntry.next[i] = prev.next[i];
 				prev.next[i] = newEntry;
 			}
@@ -82,43 +96,43 @@ public class SkipList<T extends Comparable<? super T>> {
 	/*
 	 * Returns a random value between (0, maxLevel) for the height of next[]
 	 */
-	int choice (int maxLevel){
+	int choice(int maxLevel) {
 		int i = 0;
 		boolean b = false;
 		Random rand = new Random();
-		while (i<maxLevel){
+		while (i < maxLevel) {
 			b = rand.nextBoolean();
-			if(b) i++;
-			else break;
+			if (b)
+				i++;
+			else
+				break;
 		}
 		return i;
 	}
-	
+
 	/*
 	 * Is x in the list?
 	 */
 	boolean contains(T x) {
 		SLE prev = find(x);
-		return (prev.next[0].element == (Integer)x);
+		return (prev.next[0].element == (Integer) x);
 	}
 
 	/*
-	 * Remove x from list; returns false if x was not in
-	 * list
+	 * Remove x from list; returns false if x was not in list
 	 */
 	boolean remove(T x) {
 		int value = (Integer) x;
 		SLE prev = find(x);
 		SLE toRemove = prev.next[0];
-		if(toRemove.element != (Integer)x){
+		if (toRemove.element != (Integer) x) {
 			return false;
-		}
-		else{
-			for(int i= 0; i< maxLevel; i++){
-				if(prev.next[i] == toRemove){
+		} else {
+			for (int i = 0; i < maxLevel; i++) {
+				if (prev.next[i] == toRemove) {
 					prev.next[i] = toRemove.next[i];
-				}
-				else break;
+				} else
+					break;
 			}
 			return true;
 		}
