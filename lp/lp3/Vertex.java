f120e2eaa3153fa5a52f55a481217d68e7fee5f0
ketcomp <gketkar08@gmail.com>
2016-03-27 10:44:23 -0500

    Now implements Index, Comparator<Vertex> overrides compare

--- lp/lp3/Vertex.java
+++ lp/lp3/Vertex.java
@@ -6,13 +6,14 @@
 
 import java.util.*;
 
-public class Vertex {
-    public int name; // name of the vertex
-    public boolean seen; // flag to check if the vertex has already been visited
-    public Vertex parent; // parent of the vertex
-    public int distance; // distance to the vertex from the source vertex
+public class Vertex implements Index, Comparator<Vertex>{
+    public int name; 		// name of the vertex
+    public boolean seen; 	// flag to check if the vertex has already been visited
+    public Vertex parent; 	// parent of the vertex
+    public int distance; 	// distance to the vertex from the source vertex
     public List<Edge> Adj, revAdj; // adjacency list; use LinkedList or ArrayList
-    public int inDegree; // First added to accommodate DAG's needs
+    public int inDegree; 	// Added to accommodate DAG's needs
+    private int index = 0; 	// To implement Index
 
     /**
      * Constructor for the vertex
@@ -22,7 +23,9 @@ public class Vertex {
      */
     Vertex(int n) {
 	name = n;
-	seen = false;
+	index = n;
+    distance = n;
+    seen = false;
 	parent = null;
 	Adj = new ArrayList<Edge>();
 	revAdj = new ArrayList<Edge>();   /* only for directed graphs */
@@ -32,6 +35,21 @@ public class Vertex {
      * Method to represent a vertex by its name
      */
     public String toString() {
-	return Integer.toString(name);
+    	return Integer.toString(name)+ " : " + index;
     }
+    @Override
+    public void putIndex(int index) {
+        this.index = index;
+    }
+
+    @Override
+    public int getIndex() {
+        return this.index;
+    }
+
+    @Override
+    public int compare(Vertex o1, Vertex o2) {
+        return o1.distance - o2.distance;
+    }
+    
 }
