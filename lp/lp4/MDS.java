d639ec35daa2a4c1b8370ccd4980c73e37906f3c
ketcomp <gketkar08@gmail.com>
2016-04-10 11:28:38 -0500

    Add Malav's code

--- lp/lp4/MDS.java
+++ lp/lp4/MDS.java
@@ -1,62 +1,71 @@
 
-
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
 public class MDS {
-	
-	class PriceCompare implements Comparator<Item>
-	{
-		public int compare(Item item1, Item item2)
-		{
-			if(item1.price < item2.price)
+
+	class PriceCompare implements Comparator<Item> {
+		public int compare(Item item1, Item item2) {
+			if (item1.price < item2.price)
 				return -1;
-			else if(item1.price > item2.price)
+			else if (item1.price > item2.price)
 				return 1;
 			else
 				return 0;
 		}
 	}
-	
-	TreeMap<Long, Item> primary= new TreeMap<Long, Item>();
-	TreeMap<Long, TreeSet<Item>> secondary= new TreeMap<Long, TreeSet<Item>>();
-    
+
+	TreeMap<Long, Item> primary = new TreeMap<Long, Item>();
+	TreeMap<Long, TreeSet<Item>> secondary = new TreeMap<Long, TreeSet<Item>>();
+
 	int insert(long id, double price, long[] description, int size) {
-	// Description of item is in description[0..size-1].
-	// Copy them into your data structure.
-	return 0;
-    }
+		// Description of item is in description[0..size-1].
+		// Copy them into your data structure.
+		return 0;
+	}
 
-    double find(long id) {
-	return 0;
-    }
+	double find(long id) {
+		return 0;
+	}
 
-    long delete(long id) {
-	return 0;
-    }
+	long delete(long id) {
+		return 0;
+	}
 
-    double findMinPrice(long des) {
-	return 0;
-    }
+	double findMinPrice(long des) {
+		TreeSet<Item> temp = secondary.get(des);
+		return (temp.first().price);
+	}
 
-    double findMaxPrice(long des) {
-	return 0;
-    }
+	double findMaxPrice(long des) {
+		TreeSet<Item> temp = secondary.get(des);
+		return (temp.last().price);
+	}
 
-    int findPriceRange(long des, double lowPrice, double highPrice) {
-	return 0;
-    }
+	int findPriceRange(long des, double lowPrice, double highPrice) {
+		TreeSet<Item> temp = secondary.get(des);
+		Iterator iter = temp.iterator();
+		Item tempitem = null;
+		int count = 0;
+		while (iter.hasNext()) {
+			tempitem = (Item) iter.next();
+			if (tempitem.price > lowPrice && tempitem.price < highPrice)
+				count++;
+		}
+		return count;
+	}
 
-    double priceHike(long minid, long maxid, double rate) {
-	return 0;
-    }
+	double priceHike(long minid, long maxid, double rate) {
+		return 0;
+	}
 
-    int range(double lowPrice, double highPrice) {
-	return 0;
-    }
+	int range(double lowPrice, double highPrice) {
+		return 0;
+	}
 
-    int samesame() {
-	return 0;
-    }
+	int samesame() {
+		return 0;
+	}
 }
